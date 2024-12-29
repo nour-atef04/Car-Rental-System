8ead13d6a84a4a7e809cec163e1764f7444c5c3f
MarlyMagued <151166061+MarlyMagued@users.noreply.github.com>
2024-12-29 18:32:52 +0200

    Update DDL.sql

--- SQL/DDL.sql
+++ SQL/DDL.sql
@@ -13,28 +13,18 @@ CREATE TABLE Customer (
     minit CHAR(1) NOT NULL,
     lname VARCHAR(50) NOT NULL,
     customer_phone VARCHAR(15) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE
+    email VARCHAR(255) NOT NULL,
+    drivers_license INT NOT NUL
 );
 
-CREATE TABLE Store (
-    store_id INT AUTO_INCREMENT primary key,
-    store_phone VARCHAR(15) NOT NULL,
-    street VARCHAR(255) NOT NULL,
-    city VARCHAR(100) NOT NULL,
-    country VARCHAR(100) NOT NULL
-);
-
-CREATE TABLE Employee (
-    store_id INT,  
+CREATE TABLE Empolyee (
+    store_id INT , 
     emp_ssn  VARCHAR(15) NOT NULL,
     email VARCHAR(255) NOT NULL,
     fname VARCHAR(50) NOT NULL,
     minit CHAR(1) NOT NULL,
     lname VARCHAR(50) NOT NULL,
-    emp_phone VARCHAR(15) NOT NULL,
-    PRIMARY KEY(emp_ssn),
-    CONSTRAINT fk_employee FOREIGN KEY (store_id) REFERENCES Store(store_id)
-    ON DELETE SET NULL ON UPDATE CASCADE
+    emp_phone VARCHAR(15) NOT NULL
 );
 
 CREATE TABLE Car (
@@ -47,19 +37,28 @@ CREATE TABLE Car (
     insurance VARCHAR(20) NOT NULL,
     store_id INT NOT NULL,
     year INT NOT NULL,
-    color VARCHAR(20) NOT NULL,
-    car_image_url VARCHAR(255)
+    color VARCHAR(20) NOT NULL
 );
 
 CREATE TABLE Order_place (
     order_id INT AUTO_INCREMENT primary key,
-    start_day TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    end_day TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    start_day TIMESTAMP NOT NULL,
+    end_day TIMESTAMP NOT NULL,
     payment_type VARCHAR(20),
     ssn VARCHAR(15) NOT NULL,
     nationality VARCHAR(50) NOT NULL,
     vid INT NOT NULL
 );
+
+CREATE TABLE Store (
+    store_id INT AUTO_INCREMENT primary key,
+    store_phone VARCHAR(15) NOT NULL,
+    street VARCHAR(255) NOT NULL,
+    city VARCHAR(100) NOT NULL,
+    country VARCHAR(100) NOT NULL
+);
+
+
 
 -- Primary and Foreign Keys
 ALTER TABLE Account
@@ -72,16 +71,27 @@ ALTER TABLE Customer
 ADD CONSTRAINT fk_customer FOREIGN KEY(email) REFERENCES Account(email) 
 ON DELETE CASCADE ON UPDATE CASCADE;
 
+ALTER TABLE Employee
+ADD CONSTRAINT pk_employee PRIMARY KEY(emp_ssn);
+
+ALTER TABLE Employee
+ADD CONSTRAINT fk_employee1 FOREIGN KEY (store_id) REFERENCES Store(store_id)
+ON DELETE SET NULL ON UPDATE CASCADE;
+
+ALTER TABLE Employee
+ADD CONSTRAINT fk_employee2 FOREIGN KEY(email) REFERENCES Account(email) 
+ON DELETE CASCADE ON UPDATE CASCADE;
+
 ALTER TABLE Car
 ADD CONSTRAINT fk_car FOREIGN KEY(store_id) REFERENCES Store(store_id) 
 ON DELETE CASCADE ON UPDATE CASCADE;
 
 ALTER TABLE Order_place
-ADD CONSTRAINT fk_order_place FOREIGN KEY(ssn, nationality) 
+ADD CONSTRAINT fk_order_place1 FOREIGN KEY(ssn, nationality) 
 REFERENCES Customer(ssn, nationality) 
 ON DELETE CASCADE ON UPDATE CASCADE;
 
 ALTER TABLE Order_place
-ADD CONSTRAINT fk_order_place_car FOREIGN KEY(vid) REFERENCES Car(vid) 
+ADD CONSTRAINT fk_order_place2 FOREIGN KEY(vid) REFERENCES Car(vid) 
 ON DELETE CASCADE ON UPDATE CASCADE;
 
